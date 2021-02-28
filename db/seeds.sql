c565867121abb27a6a1c0cc910b32895a7442d6e
binayaluitel2 <binayababuluitel@gmail.com>
2021-02-28 00:11:29 -0500

    Adding Seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Imititation Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', FALSE);
